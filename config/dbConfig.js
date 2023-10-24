668fcc32c368d58ee197a8faffa2d8636df53665
Ahmed Saber <ahmedsaber298@gmail.com>
2023-10-24 22:50:30 +0300

    Changed DB connection string

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 function connectDB(){
-    mongoose.connect('mongodb://localhost:27017/schoolManagement');
+    mongoose.connect('mongodb+srv://ahmadsaber298:G44ZfFWp5xg2FXOd@cluster0.psekrmx.mongodb.net/schoolManagement?retryWrites=true&w=majority');
     mongoose.connection.once('connected', () => console.log('Connected To DB'));
 }
 
